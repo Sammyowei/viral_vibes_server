da6bf8212d12c5aaa8f0d1ae9d5630f6ac667b00
Samuelson Owei <97484454+Sammyowei@users.noreply.github.com>
2024-01-08 20:36:35 +0100

    testing ipd

--- routes/index.dart
+++ routes/index.dart
@@ -2,7 +2,7 @@ import 'package:dart_frog/dart_frog.dart';
 
 Response onRequest(RequestContext context) {
   final request = context.request;
-  final port = request.connectionInfo.remoteAddress.address;
+  final port = request.connectionInfo.remoteAddress.rawAddress;
   return Response(
     body: 'Viral vibe endpoint @ api.viralvibe.hawkitpro.com : $port',
   );
